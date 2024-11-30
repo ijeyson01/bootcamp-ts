f1df0e7ad898bb9afb2192b2090a2c227298d849
Jeyson Jaramillo <jjaramillo@viamatica.com>
2024-11-30 12:43:42 -0500

    Ejercicios 30-11

---
+++ src/exceptions/02-exceptions-task.ts
@@ -0,0 +1,3 @@
+// 1. Listar a la persona con más viajes
+
+// 2. Mostrar los todos los datos de viaje de una persona con la siguiente estructura : { "persona": "nombrePersona", "origen": "descripcionOrigen", "destino": "descripcionDestino"}
